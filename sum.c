52d1241c518c0d15e0bfdfb3cf77c5e60b93f610
SARTHAK BHATT <87993262+BHATTz@users.noreply.github.com>
2023-06-15 20:04:47 +0530

    1

--- sum.c
+++ sum.c
@@ -1,11 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    int a,b;
+    int a, b;
     printf("enter the number a\n");
-    scanf("%d",&a);
+    scanf("%d", &a);
     printf("enter the number b\n");
-    scanf("%d",&b);
-    printf("sum of the number is %d\n",a+b);
+    scanf("%d", &b);
+    printf("sum of the number is %d\n", a + b);
     return 0;
 }
