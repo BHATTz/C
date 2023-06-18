ef942c0d9de560ee533e34a3e0d8fb6c1077d5be
SARTHAK BHATT <87993262+BHATTz@users.noreply.github.com>
2023-06-18 07:18:09 +0530

    1

---
+++ table.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int a,i;
+    printf("ENTER A NUMBER FOR TABLE : ");
+    scanf("%d",&a);
+    printf("TABLE OF %d\n",a);
+    for(i=1;i<=10;i++)
+    {
+      printf("%d*%d=%d \n",a,i,(a*i));
+    }
+    return 0;
+}
