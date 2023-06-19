abff4fbe973e539e1067b34b83fb81f3058faae9
SARTHAK BHATT <87993262+BHATTz@users.noreply.github.com>
2023-06-19 23:49:32 +0530

    1

---
+++ loops.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+int main()
+{
+    //     int n, sum = 0;
+    //     do
+    //     {
+    // printf("Enter a number: ");
+    // scanf("%i", &n);
+    //         sum += n;
+    //     }
+    //     while(n != 0);
+    //     printf("Sum is = %d",sum);
+
+    // int num, index = 0;
+    //     printf("Enter a number\n");
+    //     scanf("%d", &num);
+    //     do
+    //     {
+    //         printf("%d\n", index + 1);
+    //         index = index + 1;
+    //     } while (index < num);
+
+    // int i = 0;
+    // while (i<54)
+    // {
+    //     printf("%d\n", i);
+    //     i = i+1;
+    // }
+
+    // int i, j=0;
+    //     for(i=0 ; i<10 ; i++)
+    //     {
+    //        printf("%d %d\n", i, j);
+    //        i++;j++;
+    //     }
+
+    int i;
+    // for(i=1;i<10;i++)
+    // printf("%d\n",i);
+
+    // do
+    // {
+    //     i++;
+    //     printf("%d\n", i);
+    // } while (i < 10);
+
+    while (i < 10)
+    {
+        printf("%d\n", i);
+        i++;
+    }
+
+// int i,age; 
+// 	for(i = 0 ; i<100 ; i++) 
+// 	{ 
+// 		printf("Iteration time %d\nEnter Your Age : ",i ); 
+// 		scanf("%d",& ); 
+// 		// if (age>10) 
+// 		// { 
+// 		// break; // Checking Break Statement 
+// 		// } 
+// 		if(age<10) 
+// 		{ continue; }
+// 		printf("Hey Guys\n"); 
+// 		printf("This code is printed coz if condition is not satisfied. \n"); 
+// 		printf("Checking Continue Statement\n\n"); // Checking Continue Statement 
+// 	} 
+
+//  printf("Hello World\n");
+//     int i, age;
+//     for (i=0; i<10; i++){
+//         printf("%d\nEnter your age\n", i);
+//         scanf("%d", &age);
+//         // if (age>10)
+//         // {
+//         //     break;
+//         // }
+//         if (age>10)
+//         {
+//             continue;
+//         }
+//         printf("we have not come accross any continue statements");
+//         printf("we have not come accross any continue statements");
+//         printf("we have not come accross any continue statements");
+//         printf("we have not come accross any continue statements");
+//         printf("Sarthak is a good boy");
+        
+//     }
+
+    return 0;
+}
