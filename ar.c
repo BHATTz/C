a8fad3b688a61a0b8ee952e143ca40afa334a161
sarthak bhatt <sarthakbhatt0@gmail.com>
2023-08-27 14:14:59 +0530

    up

---
+++ ar.c
@@ -0,0 +1,61 @@
+// #include <stdio.h>
+
+// void main() 
+// {
+//   int ar, x=2, y=2;
+//   ar = x * y;
+//   printf("\narea of the rectangle is %d\n", ar);
+// }
+
+
+// #include <stdio.h>
+
+// void main() 
+// {
+//   int ar, x, y;
+//   printf("enter length of a rectangle : ");
+//   scanf("%d", &x);
+//   printf("enter width of a rectangle : ");
+//   scanf("%d", &y);
+//   ar = x * y;
+//   printf("\narea of the rectangle is %d\n", ar);
+// }
+
+
+// #include <stdio.h>
+
+// void main() 
+// {
+//   int r,h;
+//   float ar, pi=3.14159265359;
+//   printf("enter the radius of a circle : ");
+//   scanf("%d", &r);
+//   printf("enter the height of a circle : ");
+//   scanf("%d", &h);
+//   ar = pi*r*r*h;
+//   printf("\narea of the rectangle is %f\n", ar);
+// }
+
+
+// #include <stdio.h>
+
+// void main() 
+// {
+//   int temp, d;
+//   printf("enter degree in celsius : ");
+//   scanf("%d", &d);
+//   temp = (d*9/5)+32;
+//   printf("\narea of the rectangle is %d\n", temp);
+// }
+
+
+#include <stdio.h>
+
+void main() 
+{
+  int temp, d;
+  printf("enter degree in celsius : ");
+  scanf("%d", &d);
+  temp = (d*9/5)+32;
+  printf("\narea of the rectangle is %d\n", temp);
+}
